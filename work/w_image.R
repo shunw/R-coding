0d07a3972bc70385e60712a5b61b067264e10462
shunw <shunw.li@outlook.com>
2015-08-08 16:27:24 +0800

    fix bug, related the col data class convertion

--- work/w_image.R
+++ work/w_image.R
@@ -1,38 +1,57 @@
-#============ change the path here============
-setwd("path")
-fl<-read.csv("data.csv")
-#============ end change the path here============
+#function to get the right character
+substrRight<-function(x, n){
+  substr(x, nchar(x)-n+1, nchar(x))
+}
+#function end
 
-colnames(fl)
+#debug tool
+#check the list index of cha in the cha_1
+which(cha%in%cha_1)
+#end debug tool
+
+#===========================================
+#=================change here=================
+#===========================================
+setwd("path")
+x_col_name<-"col name will show as the x axis"
+y_col_name<-"col name will show as the y axis"
+spec_l<--0.008
+spec_h<-0.008
+#===========================================
+#=================END=======================
+#===========================================
+
+fl<-read.csv("data.csv")
 fl_clean<-fl[,-(98:168)]
 
-#============ change to value/colname you want here============
-col_name<-"colname_x_axis"
-X_axis<-quote(colname_x_axis)
-y_axis<-quote(colname_y_axis)
-spec_l<-real spec low
-spec_h<-real spec high
-#============ end change to value/colname you want here============
-
 #just select the leading edge deviation
-LD<-subset(fl, select=c(eval(X_axis), eval(y_axis)))
+LD<-subset(fl, select=c(x_col_name, y_col_name))
 
-#remove the na rows
-attach(LD)
-LD_clean<-LD[!is.na(eval(y_axis)), ]
-detach(LD)
+#remove the na rows and move the "" row in x_axis col
+LD_clean<-LD[!is.na(LD[, y_col_name]) & LD[, x_col_name]!="", ]
+
+#==========data class convert in general formate=========
+# change the character into numeric by col together
+LD_clean[, y_col_name]<-as.character(LD_clean[, y_col_name])
+# convert the % part
+
+LD_clean[substrRight(LD_clean[, y_col_name], 1)=="%", y_col_name]<-
+  as.numeric(sub("%", "", LD_clean[substrRight(LD_clean[, y_col_name], 1)=="%", y_col_name]))/100
+
+LD_clean[, y_col_name]<-as.numeric(LD_clean[, y_col_name])
+
+#==========end data class convert in general formate=========
 
 #restructure the data.frame
 library(plyr)
-LD_summary<-ddply(LD_clean, .(eval(X_axis)), 
-                   summarize, mean=mean(eval(y_axis)), sd=sd(eval(y_axis)), 
+LD_summary<-ddply(LD_clean, .(tbd=LD_clean[, x_col_name]), 
+                   summarize, mean=mean(LD_clean[, y_col_name]), sd=sd(LD_clean[, y_col_name]), 
                    meanp3sd=mean+3*sd, meanm3sd=mean-3*sd, 
                    spec_L=spec_l, spec_H=spec_h)
-LD_summary<-rename(LD_summary, c("eval(X_axis)"=col_name))
-head(LD_summary)
+LD_summary<-rename(LD_summary, c("tbd"=col_name))
+
 #remove the NA item
 LD_sum_c<-LD_summary[!is.na(LD_summary$sd),]
-tail(LD_sum_c)
 
 #draw the dots with the spec
 library(ggplot2)
